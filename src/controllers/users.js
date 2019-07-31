a10a2eac99e7e580d3d90ade040491b74a296869
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-07-31 02:54:47 -0400

    GET /api/users/:id/posts

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -24,7 +24,21 @@ function one(req, res) {
     });
 }
 
+async function posts(req, res, next) {
+  try {
+    const posts = await db.getUserPosts(req.user.id);
+
+    if (posts.length === 0)
+      next({ code: 404, message: "User hasn't made any posts." });
+    else
+      res.status(200).json({ posts, success: true });
+  } catch (error) {
+    next({ code: 500, message: "User's posts could not be retrieved." });
+  }
+}
+
 module.exports = {
   all,
-  one
+  one,
+  posts 
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,6 @@ const { validateUserId } = require('../middleware/validation');
 
 router.get('/', users.all);
 router.get('/:id', validateUserId, users.one);
+router.get('/:id/posts', validateUserId, users.posts);
 
 module.exports = router;
